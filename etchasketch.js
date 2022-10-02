ba22d00a64a1aa444e2584c18f992b2b045f0d2b
Jenny Dixon <jennyrmdixon@gmail.com>
2022-10-02 15:45:30 -0700

    Set upper limit for number entry and code clean up

--- etchasketch.js
+++ etchasketch.js
@@ -1,37 +1,40 @@
 //Allow prompt on click
 let button = document.querySelector("#buttonArea > button")
 button.addEventListener('click', () => {
-    let gridSize = prompt("Please enter a number",)
-    makeGrid(gridSize);
+    let gridSize = parseInt(prompt("Please enter a number",));
+    if (gridSize > 100) {
+        alert("That number is too large, please try again with 100 or less.");
+    }
+    else {
+        makeGrid(gridSize);
+    }
 });
 
 const gridArea = document.querySelector('#gridArea');
 
-function makeGrid(gridSize){
+function makeGrid(gridSize) {
 
-//Clear previous grid
-while (gridArea.firstChild) {
-    gridArea.removeChild(gridArea.lastChild);
+    //Clear previous grid
+    while (gridArea.firstChild) {
+        gridArea.removeChild(gridArea.lastChild);
     }
 
-//Make new grid
-for (let i = 0; i < gridSize; i++) {
-    let gridRow = document.createElement('div');
-    gridArea.appendChild(gridRow); 
-    gridRow.setAttribute('class', 'gridRow');  
+    //Make new grid
     for (let i = 0; i < gridSize; i++) {
-        let gridBox = document.createElement('div');
-        gridRow.appendChild(gridBox); 
-        gridBox.setAttribute('class', 'gridBox');                              
-    } 
- //Try to get it not with java?
-    //Try setting classes at the end?
+        let gridRow = document.createElement('div');
+        gridArea.appendChild(gridRow);
+        gridRow.setAttribute('class', 'gridRow');
+        for (let i = 0; i < gridSize; i++) {
+            let gridBox = document.createElement('div');
+            gridRow.appendChild(gridBox);
+            gridBox.setAttribute('class', 'gridBox');
+        }
 
-    //Set hover
-    let boxes = gridArea.querySelectorAll('.gridBox');
-    boxes.forEach(box => box.addEventListener('mouseover', () => {
-    box.classList.add('boxHover');                               
-  }));                         
-}
+        //Set hover
+        let boxes = gridArea.querySelectorAll('.gridBox');
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            box.classList.add('boxHover');
+        }));
+    }
 }
 
