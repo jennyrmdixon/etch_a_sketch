f4a1f970d509c1f760931f8a5f7f80b34c86873e
Jenny Dixon <jennyrmdixon@gmail.com>
2022-10-02 15:38:16 -0700

    Allow user to change grid size.

--- etchasketch.js
+++ etchasketch.js
@@ -1,30 +1,37 @@
-const gridarea = document.querySelector('#gridarea');
+//Allow prompt on click
+let button = document.querySelector("#buttonArea > button")
+button.addEventListener('click', () => {
+    let gridSize = prompt("Please enter a number",)
+    makeGrid(gridSize);
+});
 
-let gridbox;
-for (let i = 0; i < 16; i++) {
-    let gridrow = document.createElement('div');
-    gridarea.appendChild(gridrow); 
-    gridrow.setAttribute('class', 'gridrow');  
-    for (let i = 0; i < 16; i++) {
-        let gridbox = document.createElement('div');
-        gridrow.appendChild(gridbox); 
-        gridbox.setAttribute('class', 'gridbox');                              
-    }                            
+const gridArea = document.querySelector('#gridArea');
+
+function makeGrid(gridSize){
+
+//Clear previous grid
+while (gridArea.firstChild) {
+    gridArea.removeChild(gridArea.lastChild);
+    }
+
+//Make new grid
+for (let i = 0; i < gridSize; i++) {
+    let gridRow = document.createElement('div');
+    gridArea.appendChild(gridRow); 
+    gridRow.setAttribute('class', 'gridRow');  
+    for (let i = 0; i < gridSize; i++) {
+        let gridBox = document.createElement('div');
+        gridRow.appendChild(gridBox); 
+        gridBox.setAttribute('class', 'gridBox');                              
+    } 
+ //Try to get it not with java?
+    //Try setting classes at the end?
+
+    //Set hover
+    let boxes = gridArea.querySelectorAll('.gridBox');
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+    box.classList.add('boxHover');                               
+  }));                         
+}
 }
 
-
-let boxes = gridarea.querySelectorAll('.gridbox');
-boxes.forEach(box => box.addEventListener('mouseover', () => {
-    box.classList.add('boxhover');                               
-  }));
-
-  //generate input
-  //create button
-  //onclick event for button - prompt comes up
-  //user enters height and width, this gets set as variables "size"
-  //"i" now gets set to "size"
-
-
-  //
-  //have grid stay at the same width
-  //
